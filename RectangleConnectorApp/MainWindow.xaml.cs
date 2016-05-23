b4996a27988a1b546c94b1c0a01bc962fb3e1415
Kasper Guldmann Nielsen <ms@kalleguld.dk>
2016-05-23 07:51:38 +0200

    Added more rectangles

--- RectangleConnectorApp/MainWindow.xaml.cs
+++ RectangleConnectorApp/MainWindow.xaml.cs
@@ -13,11 +13,14 @@ namespace RectangleConnectorApp
                 {
                     new Rectangle(new Coordinate(100,100), new Coordinate(150,150), Color.Black),
                     new Rectangle(new Coordinate(50,50), new Coordinate(100,175), Color.Blue),
-
+                    new Rectangle(new Coordinate(200,200), new Coordinate(300,300),new Color(255,255,0)),
+                    new Rectangle(new Coordinate(200,0), new Coordinate(300,100),new Color(255,0,255)),
                 },
                 new List<Rectangle>
                 {
-                        new Rectangle(new Coordinate(30,60), new Coordinate(60,300), Color.Red),
+                    new Rectangle(new Coordinate(30,60), new Coordinate(60,300), Color.Red),
+                    new Rectangle(new Coordinate(200,0), new Coordinate(300,100), new Color(127,127,127)),
+                    new Rectangle(new Coordinate(200,200), new Coordinate(300,300), Color.Green),
                 });
 
             InitializeComponent();
